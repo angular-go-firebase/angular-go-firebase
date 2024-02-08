e5f485b46e4870f909b627ba47d80e28e0fc48c5
Paralaxe <andre.bagatelli@paralaxe.com.br>
2024-02-08 11:08:04 -0300

    sales ok

--- api/main.go
+++ api/main.go
@@ -44,6 +44,8 @@ func main() {
 	// Rotas
 	router.POST("api/add-product", newProduct)
 	router.GET("api/get-products-list", getProducts)
+	router.GET("api/get-sales-list", getSales)
+	router.POST("api/delete-product", deleteProduct)
 
 	// Iniciando servidor
 	router.Run(":8080")
@@ -112,6 +114,74 @@ func getProducts(c *gin.Context) {
 
 		// Mapear dados do documento para um mapa
 		data := doc.Data()
+		data["ID"] = doc.Ref.ID
+		result = append(result, data)
+
+		fmt.Printf("Dados do Firestore: %v\n", data)
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func deleteProduct(c *gin.Context) {
+	client := initFirebase()
+	defer client.Close()
+
+	var ID string
+
+	if err := c.BindJSON(&ID); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro": "Erro ao decodificar o objeto JSON"})
+		return
+	}
+
+	// Obter referência para o documento na coleção "Products"
+	docRef := client.Collection("Products").Doc(ID)
+
+	// Obter os dados do documento
+	docSnapshot, err := docRef.Get(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"erro": "Erro ao obter o documento"})
+		return
+	}
+
+	// Salvar os dados na coleção "Sales"
+	if _, err := client.Collection("Sales").Doc(ID).Set(ctx, docSnapshot.Data()); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"erro": "Erro ao mover o documento para a coleção Sales"})
+		return
+	}
+
+	// Remover o documento da coleção "Products"
+	if _, err := docRef.Delete(ctx); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"erro": "Erro ao remover o documento da coleção Products"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Produto movido com sucesso para a coleção Sales"})
+}
+
+func getSales(c *gin.Context) {
+	client := initFirebase()
+	defer client.Close()
+	// Buscar dados do Firestore
+	iter := client.Collection("Sales").Documents(ctx)
+	defer iter.Stop()
+
+	var result []map[string]interface{}
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Erro ao iterar documentos: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Erro ao buscar dados"})
+			return
+		}
+
+		// Mapear dados do documento para um mapa
+		data := doc.Data()
+		data["ID"] = doc.Ref.ID
 		result = append(result, data)
 
 		fmt.Printf("Dados do Firestore: %v\n", data)
